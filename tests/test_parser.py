dfcc7af2cf4f63226450c597e9ef0696ec7fcf7b
CoderAryanAnand <68732506+CoderAryanAnand@users.noreply.github.com>
2022-07-17 23:31:22 +0200

    parser unittest

---
+++ tests/test_parser.py
@@ -0,0 +1,87 @@
+import unittest
+from TechZen.token_ import Token, TokenType
+from TechZen.lexer_ import Lexer
+from TechZen.parser_ import Parser
+from TechZen.nodes_ import *
+
+
+class TestParser(unittest.TestCase):
+
+    def test_empty(self):
+        node = Parser(Lexer("<stdin>", "").make_tokens()[0]).parse().node
+        self.assertEqual(node, None)
+
+    def test_numbers(self):
+        node = Parser(Lexer("<stdin>", "51.2").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes), "[FLOAT:51.2]")
+
+    def test_single_operations(self):
+
+        # ADDITION
+        node = Parser(Lexer("<stdin>", "27 + 14").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes), "[(INT:27, PLUS, INT:14)]")
+
+        # SUBTRACTION
+        node = Parser(Lexer("<stdin>", "27 - 14").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes), "[(INT:27, MINUS, INT:14)]")
+
+        # MULTIPLICATION
+        node = Parser(Lexer("<stdin>", "27 * 14").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes), "[(INT:27, MUL, INT:14)]")
+
+        # DIVISION
+        node = Parser(Lexer("<stdin>", "27 / 14").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes), "[(INT:27, DIV, INT:14)]")
+
+        # FLOOR
+        node = Parser(Lexer("<stdin>", "27 // 14").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes), "[(INT:27, DFL, INT:14)]")
+
+        # MODULO
+        node = Parser(Lexer("<stdin>", "27 % 14").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes), "[(INT:27, MOD, INT:14)]")
+
+    def test_full_expression(self):
+        node = Parser(Lexer("<stdin>", "27 + (43 / 36 - 38) * 51").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes),
+                         "[(INT:27, PLUS, (((INT:43, DIV, INT:36), MINUS, INT:38), MUL, INT:51))]")
+
+    def test_if_statement(self):
+        node = Parser(Lexer("<stdin>", "if TRUE then\nprint('hi')\nelse\nprint('bye')\nend").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes[0])[:33],
+                         "<TechZen.nodes_.IfNode object at ")
+
+    def test_for_loop(self):
+        node = Parser(Lexer("<stdin>", "for i = 0 to 5 then\nprint(i)\nend").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes[0])[:34],
+                         "<TechZen.nodes_.ForNode object at ")
+
+    def test_while_loop(self):
+        node = Parser(Lexer("<stdin>", "while TRUE then\nprint('hi'); break\nend").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes[0])[:36],
+                         "<TechZen.nodes_.WhileNode object at ")
+
+    def test_func_def(self):
+        node = Parser(Lexer("<stdin>", "fun add(a,b) -> a + b").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes[0])[:38],
+                         "<TechZen.nodes_.FuncDefNode object at ")
+
+    def test_try_expr(self):
+        node = Parser(Lexer("<stdin>", "try; print('a'); except; print('b'); end").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes[0])[:34],
+                         "<TechZen.nodes_.TryNode object at ")
+
+    def test_class_def(self):
+        node = Parser(Lexer("<stdin>", "class a; fun a(b); print(b); end; end;").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes[0])[:36],
+                         "<TechZen.nodes_.ClassNode object at ")
+
+    def test_list_expr(self):
+        node = Parser(Lexer("<stdin>", "[]").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes[0])[:35],
+                         "<TechZen.nodes_.ListNode object at ")
+
+    def test_dict_expr(self):
+        node = Parser(Lexer("<stdin>", "{}").make_tokens()[0]).parse().node
+        self.assertEqual(str(node.element_nodes[0])[:35],
+                         "<TechZen.nodes_.DictNode object at ")
