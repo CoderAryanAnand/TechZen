cb1ea685a9d53cd80633ed56724fef330579441a
CoderAryanAnand <68732506+CoderAryanAnand@users.noreply.github.com>
2022-07-19 13:55:26 +0200

    Context docstring

--- TechZen/context_.py
+++ TechZen/context_.py
@@ -5,6 +5,12 @@
 
 class Context:
     def __init__(self, display_name, parent=None, parent_entry_pos=None):
+        """
+        This provides context (the parent file, the current function, etc...) for error messages.
+        :param display_name: File, input or function name
+        :param parent: Name of the parent input, e.g. file
+        :param parent_entry_pos: Position where the child function or file is called
+        """
         self.display_name = display_name
         self.parent = parent
         self.parent_entry_pos = parent_entry_pos
