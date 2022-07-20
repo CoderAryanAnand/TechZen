8614551fe4ed4426773a2cbdcaedfa2ab7697c11
CoderAryanAnand <68732506+CoderAryanAnand@users.noreply.github.com>
2022-07-20 21:27:20 +0200

    string docstring

--- TechZen/types/string_.py
+++ TechZen/types/string_.py
@@ -4,22 +4,41 @@ from TechZen.types.number_ import Number
 
 class String(Value):
     def __init__(self, value):
+        """
+        String type. Inherits from Value class.
+        :param value: String
+        """
         super().__init__()
         self.value = value
 
     def added_to(self, other):
+        """
+        Concatenate two strings.
+        :param other: String
+        :return: Updated string
+        """
         if isinstance(other, String):
             return String(self.value + other.value).set_context(self.context), None
         else:
             return None, Value.illegal_operation(self, other)
 
     def multed_by(self, other):
+        """
+        Multiply a string.
+        :param other: Number
+        :return: Updated string
+        """
         if isinstance(other, Number):
             return String(self.value * other.value).set_context(self.context), None
         else:
             return None, Value.illegal_operation(self, other)
 
     def get_comparison_eq(self, other):
+        """
+        Compare two strings for the equal to comparison.
+        :param other: String
+        :return: Boolean
+        """
         if isinstance(other, String):
             return (
                 Number(int(self.value == other.value)).set_context(self.context),
@@ -29,6 +48,11 @@ class String(Value):
             return None, Value.illegal_operation(self, other)
 
     def get_comparison_ne(self, other):
+        """
+        Compare two strings for the not equal to comparison.
+        :param other: String
+        :return: Boolean
+        """
         if isinstance(other, String):
             return (
                 Number(int(self.value != other.value)).set_context(self.context),
@@ -38,9 +62,17 @@ class String(Value):
             return None, Value.illegal_operation(self, other)
 
     def is_true(self):
+        """
+        Decide whether the string contains any characters.
+        :return: Boolean
+        """
         return len(self.value) > 0
 
     def copy(self):
+        """
+        Make a copy of the string.
+        :return: Copy
+        """
         copy = String(self.value)
         copy.set_pos(self.pos_start, self.pos_end)
         copy.set_context(self.context)
