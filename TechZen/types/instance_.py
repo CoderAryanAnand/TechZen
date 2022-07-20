24ff44518737caa288dd8f9fa9f0148a5d0f2436
CoderAryanAnand <68732506+CoderAryanAnand@users.noreply.github.com>
2022-07-20 21:08:15 +0200

    instance docstring

--- TechZen/types/instance_.py
+++ TechZen/types/instance_.py
@@ -3,11 +3,19 @@ from TechZen.types.value_ import Value
 
 class Instance(Value):
     def __init__(self, parent_class):
+        """
+        Instance class. Inherits from Value class.
+        :param parent_class: Parent class
+        """
         super().__init__()
         self.parent_class = parent_class
         self.symbol_table = None
 
     def copy(self):
+        """
+        Make a copy of instance.
+        :return: Copy
+        """
         return self
 
     def __repr__(self):
